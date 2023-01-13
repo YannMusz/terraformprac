31a082cf98600adba4fb55711faa4bfe90b3cb1a
Yann <yann.musz@gmail.com>
2023-01-12 18:21:01 -0600

    s3bucket

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-pracc"
+    key    = "terraform-pracc/dev"
+    region = "us-east-2"
+  }
+}
+
+
+
 provider "aws" {
     region = var.region
 }
